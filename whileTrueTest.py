b19d06f73cae9cbc7113efab4f01a39ef476b71e
Jade Zhan <zhanzixuan@outlook.com>
2020-06-29 22:17:45 +0800

    change to while i > 0

--- whileTrueTest.py
+++ whileTrueTest.py
@@ -9,7 +9,7 @@
 
 password = 'a123456' #数字的东西通常存一次就好
 i = 3 #回圈次数
-while True:
+while True:  #或写成 while i > 0:
 	pwd = input('请输入密码： ')
 	if pwd == password:
 		print('登录成功！')
@@ -20,3 +20,15 @@ while True:
 		if i == 0: #剩余机会是0的时候break
 			break
 
+# =================或者写成================================
+password = 'a123456' #数字的东西通常存一次就好
+i = 3 #回圈次数
+while i > 0:
+	pwd = input('请输入密码： ')
+	if pwd == password:
+		print('登录成功！')
+		break
+	else:
+		i = i - 1 #最多输入3次
+		print('密码错误！还有', i, '次机会！') #注意看，很讲究，内容用逗号隔开
+		
