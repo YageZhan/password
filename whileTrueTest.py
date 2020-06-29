f084b6c4f067ebd83d8a3c35ac643b235a772802
Jade Zhan <zhanzixuan@outlook.com>
2020-06-29 22:23:54 +0800

    optimizetion

--- whileTrueTest.py
+++ whileTrueTest.py
@@ -24,11 +24,15 @@ while True:  #或写成 while i > 0:
 password = 'a123456' #数字的东西通常存一次就好
 i = 3 #回圈次数
 while i > 0:
+	i = i - 1 #最多输入3次，减少剩余次数；每问一次密码就减少一次机会
 	pwd = input('请输入密码： ')
 	if pwd == password:
 		print('登录成功！')
 		break
 	else:
-		i = i - 1 #最多输入3次
-		print('密码错误！还有', i, '次机会！') #注意看，很讲究，内容用逗号隔开
+		print('密码错误!')
+		if i > 0:
+			print('还有', i, '次机会！')
+		else:
+			print('没有机会了，要锁帐号了！')
 		
